dc0ad90eb175271afb7542a40aa827bddef20fa8
Gabriel Teles <bilebile2020@gmail.com>
2026-03-18 17:25:11 -0300

    Modifica CORS

--- dist/index.js
+++ dist/index.js
@@ -16,7 +16,6 @@ app.use((0, cors_1.default)({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", (0, cors_1.default)());
 app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
