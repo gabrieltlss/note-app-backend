878d4a4567522265495410b4e32de9613b849586
Gabriel Teles <bilebile2020@gmail.com>
2026-03-18 17:22:26 -0300

    Modifica CORS

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+app.options("*", cors());
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
