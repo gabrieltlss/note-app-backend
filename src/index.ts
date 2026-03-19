7eb58151f84b6f256dfa255d37a4cc9fa94ff6d6
Gabriel Teles <bilebile2020@gmail.com>
2026-03-19 09:36:21 -0300

    Modifica CORS

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
     origin: `${process.env.ORIGIN_URL}`,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(cookieParser());
