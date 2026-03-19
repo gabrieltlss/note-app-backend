0a15ff415d7477c8faba56b4d47ee76c713f4d91
Gabriel Teles <bilebile2020@gmail.com>
2026-03-19 10:37:22 -0300

    Modifica CORS

--- dist/index.js
+++ dist/index.js
@@ -10,14 +10,14 @@ dotenv_1.default.config();
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.json());
 app.use((0, cors_1.default)({
     origin: `${process.env.ORIGIN_URL}`,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.use((0, cookie_parser_1.default)());
-app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.use(routes_1.default);
 app.use((error, req, res, next) => {
