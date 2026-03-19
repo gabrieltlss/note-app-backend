5951f1e7469599730e8b7c415fc8813c1a104cdb
Gabriel Teles <bilebile2020@gmail.com>
2026-03-19 10:05:37 -0300

    Revert "Modifica CORS"
    This reverts commit df869a06ecd1aa34e07e8e4b657aaef3614dba46.

--- dist/index.js
+++ dist/index.js
@@ -11,9 +11,9 @@ const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)({
-    origin: "*",
+    origin: `${process.env.ORIGIN_URL}`,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use((0, cookie_parser_1.default)());
--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,9 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 app.use(cors({
-    origin: "*",
+    origin: `${process.env.ORIGIN_URL}`,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(cookieParser());
