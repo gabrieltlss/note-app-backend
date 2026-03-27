2129f4438f19a6973d24513e8469092e97d79494
Gabriel Teles <bilebile2020@gmail.com>
2026-03-27 12:54:24 -0300

    Modifica posição do dotenv

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const routes_1 = __importDefault(require("./routes"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const app = (0, express_1.default)();
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import router from "./routes";
 import dotenv from "dotenv";
 dotenv.config();
+import express from "express";
+import router from "./routes";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
