704263d162e511e9cb24a576bc5f922d1377a785
Gabriel Teles <bilebile2020@gmail.com>
2026-03-27 14:15:38 -0300

    Configurações de BD

--- src/models/database.ts
+++ src/models/database.ts
@@ -6,7 +6,6 @@ const connectionData = {
     user: String(process.env.DB_USER),
     password: String(process.env.DB_PASSWORD),
     database: String(process.env.DB_DATABASE),
-    port: Number(process.env.PORT),
     host: String(process.env.HOST)
 }
 
